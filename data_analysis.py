7bbe5354b55bbc59c3c97ff2fd856258b14a4563
Paulo Ribeiro <paulo.ribeirodecarvalho@epfl.ch>
2024-11-27 15:05:19 +0100

    adding architecture for the data analysis first part

---
+++ data_analysis.py
@@ -0,0 +1,21 @@
+"""
+Python script used to define function allowing easy data analysis.
+"""
+
+import numpy as np
+
+
+class TweetsAnalyzer:
+    def __init__(self):
+        ...
+
+    def func1(self):
+        ...
+
+
+class BTCAnalyzer:
+    def __init__(self):
+        ...
+
+    def func1(self):
+        ...
