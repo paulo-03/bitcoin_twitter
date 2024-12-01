cabb8e4f5eb772838925b2f6255d6354051bf945
Paulo Ribeiro <paulo.ribeirodecarvalho@epfl.ch>
2024-12-01 20:16:47 +0100

    Data analysis completed

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 This repository propose our analysis of the influence from twitter posts over the bitcoin price.
 
 ***Authors:*** [Rami Atassi](mailto:rami.atassi@epfl.ch), [Mahmoud Dokmak](mahmoud.dokmak@epfl.ch) & [Paulo Ribeiro](paulo.ribeirodecarvalho@epfl.ch).
+
+## Objective for Bitcoin and Twitter Data Analysis (2017–2019)
+
+We propose the following steps for analyzing the correlation between Bitcoin prices and Twitter activity during 2017–2019.  
+
+### 1. Bitcoin Price Analysis  
+- **Focus on Returns**: Instead of analyzing the price directly, we will compute Bitcoin returns to identify significant trends and events.  
+- **Highlight Big Events**: Identify key moments, such as major price spikes or drops, within the timeframe.  
+
+### 2. Twitter Data Analysis  
+- **Define Influential Tweets**: Filter tweets based on defined thresholds (e.g., x likes, replies, or retweets) to focus on medium/highly impactful content.  
+- **Language Filter**: Ensure only English tweets are included, as non-English content (~5-10%) can be considered negligible.  
+- **Analyze Influence**: Highlight accounts with the highest engagement (likes/retweets) for easier representation in the final visualizations.  
+
+### 3. Cross-Analysis Objective  
+- **Correlation Analysis**: Investigate whether:  
+  - Bitcoin price events (returns) trigger Twitter discussions, or  
+  - Influential tweets anticipate significant Bitcoin price changes.  
+- **Effect Duration**: If Twitter is found to influence Bitcoin prices, estimate the duration of this effect.  
+
+The ultimate goal is to determine the direction and strength of this relationship, if any. A sample visualization will illustrate the influence of key accounts and events.  
--- data_analysis.py
+++ data_analysis.py
@@ -2,15 +2,18 @@
 Python script used to define function allowing easy data analysis.
 """
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
+import datetime
 
 
 class TweetsAnalyzer:
     def __init__(self, file_path: str):
         self.data = self._load_data_in_chunk(file_path)
+        self._basic_info()
 
     @staticmethod
     def _load_data_in_chunk(file_path, chunk_size: int = 1000000) -> pd.DataFrame:
@@ -25,19 +28,28 @@ class TweetsAnalyzer:
                           total=int(16889765 / chunk_size),
                           unit='chunks'):
             df = pd.concat([df, chunk])  # Append chunk to the DataFrame
-        return df
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        return df.sort_values(by='timestamp', ascending=True).reset_index()
+
+    def _basic_info(self):
+        """Print basic information at the initialization of the class TweetsAnalyzer"""
+        print(f"{'#' * 10} Basic information {'#' * 10}\n"
+              f"- Shape of dataset: {self.data.shape}\n"
+              f"- Data starting from {self.data['timestamp'].min()} to {self.data['timestamp'].max()}\n"
+              f"- Data Types of each column:\n{self.data.dtypes}")
 
     def performance_distribution(self) -> pd.DataFrame:
         """Plot the distribution of the likes, replies and retweet column to assess the influence of a tweet"""
         # Set up the figure
-        fig, axes = plt.subplots(3, 1, figsize=(8, 12), sharex=True)
+        fig, axes = plt.subplots(3, 1, figsize=(8, 12), sharex=False)
 
         colors = ['blue', 'green', 'orange']
         metrics = ['likes', 'replies', 'retweets']
 
         for idx, (color, metric) in enumerate(zip(colors, metrics)):
             # Plot distributions
-            sns.histplot(self.data[metric], ax=axes[idx], color=color)
+            sns.histplot(self.data[metric], ax=axes[idx], color=color,
+                         bins=range(0, self.data[metric].max() + 100, 100))
             axes[idx].set_title(f'Distribution of {metric}')
             axes[idx].set_ylabel('Frequency')
             axes[idx].set_yscale('log')  # Set y-axis to logarithmic scale
@@ -46,23 +58,66 @@ class TweetsAnalyzer:
         plt.tight_layout()
         plt.show()
 
-        return pd.concat([self.data['likes'].describe(percentiles=[0.89, 0.99]),
-                          self.data['replies'].describe(percentiles=[0.95, 0.99]),
-                          self.data['retweets'].describe(percentiles=[0.88, 0.99])],
+        return pd.concat([self.data['likes'].describe(percentiles=np.linspace(start=0.9, stop=0.99, num=10)),
+                          self.data['replies'].describe(percentiles=np.linspace(start=0.9, stop=0.99, num=10)),
+                          self.data['retweets'].describe(percentiles=np.linspace(start=0.9, stop=0.99, num=10))],
                          axis=1)
+
+    def pertinent(self, start_date: datetime, end_date: datetime):
+        """Plot a graph to easily see the distribution of pertinent/meaningful/impactful tweets meaning having more
+        than at least 10 likes/replies/retweets"""
+        filtered_data = self.data[(self.data['timestamp'] >= start_date) & (self.data['timestamp'] < end_date)]
+        thresholds = np.linspace(start=10, stop=500, num=50)
+        tweet_number = []
+
+        for threshold in tqdm(thresholds, desc="Computing the number of tweet per threshold of perf", unit="threshold"):
+            tweet_number.append((filtered_data[['likes', 'replies', 'retweets']] > threshold).any(axis=1).sum())
+
+        plt.figure(figsize=(15, 6))
+        plt.plot(thresholds, tweet_number)
+
+        plt.show()
+
 
 class BTCAnalyzer:
     def __init__(self, file_path: str):
-        self.data = self.load_data(file_path)
+        self.data = self._load_data(file_path)
+        self.data.index = pd.to_datetime(self.data.index).tz_localize('UTC')  # Make sure the time zone is UTC
+        self._basic_info()
 
-    def load_data(self, file_path: str) -> pd.DataFrame:
+    @staticmethod
+    def _load_data(file_path: str) -> pd.DataFrame:
         return pd.read_parquet(file_path)
 
-    def plot_price(self):
-        fig, ax = plt.subplots(figsize=(10, 8))
-        ax.plot(self.data.index, self.data['high'])
+    def _basic_info(self):
+        """Print basic information at the initialization of the class BTCAnalyzer"""
+        print(f"{'#' * 10} Basic information {'#' * 10}\n"
+              f"- Shape of dataset: {self.data.shape}\n"
+              f"- Average time granularity: {self.data.index.to_series().diff().mean()} (~1 min granularity)\n"
+              f"- Data starting from {self.data.index.min()} to {self.data.index.max()}\n"
+              f"- Data Types of each column:\n{self.data.dtypes}")
+
+    def _compute_return(self):
+        """Private function to compute returns of bitcoin instead of only having its price"""
+        self.data['return'] = self.data['close'].pct_change() * 100
+
+    def plot_price(self, start_date: datetime, end_date: datetime):
+        """Plot the price evolution of the bitcoin and allows the user to select the time range he wants"""
+        filtered_price = self.data[(self.data.index >= start_date) & (self.data.index < end_date)]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(filtered_price.index, filtered_price['close'])
         ax.set_title('BTC Price over time')
         ax.set_xlabel('Date')
         ax.set_ylabel('Price in $')
         plt.show()
 
+    def plot_return(self, start_date: datetime, end_date: datetime):
+        """Plot the return evolution of the bitcoin and allows the user to select the time range he wants"""
+        self._compute_return()
+        filtered_return = self.data[(self.data.index >= start_date) & (self.data.index < end_date)]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(filtered_return.index, filtered_return['return'])
+        ax.set_title('BTC Return over time')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Return in %')
+        plt.show()
