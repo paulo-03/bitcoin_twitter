1076e4ebd2020a5c8f58d7746a502ffb6b2e6050
RamiATASSI <114172598+RamiATASSI@users.noreply.github.com>
2025-01-08 19:26:35 +0100

    Transfer Entropy plot on real data based on delay

--- helpers/transfer_entropy.py
+++ helpers/transfer_entropy.py
@@ -1,28 +1,36 @@
-'''
+"""
 Class for computing the transfer entropy between the time series of bitcoin returns classified [Bearish, Neutral, Bullish]
 and the tweets sentiment classified [Bearish, Neutral, Bullish].
-'''
+"""
 
-from pyinform.transferentropy import transfer_entropy
-import numpy as np
-from scipy.stats import chi2
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+from pyinform.transferentropy import transfer_entropy
+from scipy.stats import chi2
+
 
 class TweetsToBtcTransferEntropy:
-    def __init__(self, btc_returns: np.ndarray, tweets_sentiment: np.ndarray):
-        self.btc_returns = btc_returns
+    def __init__(self, tweets_sentiment: np.ndarray, btc_returns: np.ndarray):
         self.tweets_sentiment = tweets_sentiment
+        self.btc_returns = btc_returns
         self.length = len(btc_returns)
         self.df = 12  # degrees of freedom = ν = nA * (nA+ - 1) * (nB - 1) = 3 * (3-1) * (3-1) = 12
 
-    def compute_transfer_entropy(self, delay: int = 1) -> float:
+    def compute_transfer_entropy(self, delay: int = 0, k: int = 1) -> float:
         """
         Compute the transfer entropy between the two time series with a given delay.
         :param delay: The lag between the two time series.
+        :param k: The history length.
         :return: The transfer entropy value.
         """
-        return transfer_entropy(self.btc_returns, self.tweets_sentiment, k=delay) #source and target are reversed in the function
+        if delay == 0:
+            source = self.tweets_sentiment
+            target = self.btc_returns
+        else:
+            source = self.tweets_sentiment[:-delay]
+            target = self.btc_returns[delay:]
+        return transfer_entropy(source=source, target=target, k=k)
 
     def compute_p_value(self, TE: float) -> float:
         """
@@ -33,44 +41,63 @@ class TweetsToBtcTransferEntropy:
         res = 2 * self.length * TE
         return 1 - chi2.cdf(res, self.df)
 
-    def plot_transfer_entropy_on_lags(self, delays: list = range(1, 15)) -> None:
+    def transfer_entropy_significance_threshold(self, alpha: float = 0.01) -> float:
+        """
+        Compute the transfer entropy significance threshold for a given alpha.
+        :param alpha: The significance level.
+        :return: The transfer entropy significance threshold.
+        """
+        return chi2.ppf(1 - alpha, self.df) / (2 * self.length)
+
+    def plot_transfer_entropy_on_lags(self, delays: list = range(1, 200), k: int = 1) -> None:
         """
         Plot the transfer entropy values for different delays.
         :param delays: The list of delays to compute the transfer entropy.
+        :param k: The history length.
         """
-        transfer_entropy_values = []
-        p_value_significant = []
-        for delay in delays:
 
-            TE = self.compute_transfer_entropy(delay=delay)
-            transfer_entropy_values.append(TE)
-            p_value_significant.append(self.compute_p_value(TE) < 0.01)
+        transfer_entropy_values = [self.compute_transfer_entropy(delay, k) for delay in delays]
 
         plt.figure(figsize=(12, 6))
-        for i in range(len(delays) - 1):
-            plt.plot(delays[i:i + 2], transfer_entropy_values[i:i + 2],
-                     color='#1f77b4' if p_value_significant[i] else '#ff7f0e',
-                     label='Significant (p < 0.01)' if p_value_significant[i] else 'Not Significant (p > 0.01)')
-
-        # Remove duplicate labels
-        handles, labels = plt.gca().get_legend_handles_labels()
-        by_label = dict(zip(labels, handles))
-        plt.legend(by_label.values(), by_label.keys())
+        sns.lineplot(x=delays, y=transfer_entropy_values, drawstyle='steps-post')
+        plt.fill_between(delays, min(transfer_entropy_values), self.transfer_entropy_significance_threshold(),
+                         color='pink', alpha=0.3, label='not significant')
 
         plt.xlabel('Time shift [hour]')
         plt.ylabel('Transfer Entropy [bit]')
-        plt.title('Transfer Entropy between Tweets Sentiment and BTC Returns')
+        plt.title('Transfer Entropy from Tweets Sentiment to BTC Returns')
+        plt.legend()
         plt.show()
 
+    def plot_transfer_entropy_on_history_lengths(self, ks: list = range(1, 17)) -> None:
+        """
+        Plot the mean transfer entropy values for different history lengths.
+        :param max_delay: The maximum delay to compute the transfer entropy.
+        :param ks: The list of history lengths to compute the transfer entropy.
+        """
+
+        transfer_entropy_values = [self.compute_transfer_entropy(0, k) for k in ks]
+
+        plt.figure(figsize=(12, 6))
+        sns.lineplot(x=ks, y=transfer_entropy_values)
+        plt.xlabel('History length (k)')
+        plt.ylabel('Transfer Entropy [bit]')
+        plt.title('Transfer Entropy for Different History Lengths')
+        plt.show()
 
 def main() -> None:
     # Example
-    seed = 42
-    np.random.seed(seed)
-    tweets_sentiment = np.random.randint(0, 3, 18000)  # Source series
-    btc_returns = np.roll(tweets_sentiment, 8)  # Target series influenced by source
-    te = TweetsToBtcTransferEntropy(btc_returns, tweets_sentiment)
-    te.plot_transfer_entropy_on_lags()
+    tweets_sentiment = np.load('../data/tweets_time_series.npy')
+    btc_returns = np.load('../data/btc_time_series.npy')
+    te = TweetsToBtcTransferEntropy(tweets_sentiment, btc_returns)
+    te.plot_transfer_entropy_on_lags(delays=list(range(0, 24)), k=1)
+
+    #Random test
+    # random1 = np.random.randint(0, 3, size=len(btc_returns))
+    # random2 = np.random.randint(0, 3, size=len(btc_returns))
+    # te_random = TweetsToBtcTransferEntropy(random1, random2)
+    # te_random.plot_transfer_entropy_on_lags(delays=list(range(1, 200)))
+
 
 if __name__ == '__main__':
     main()
